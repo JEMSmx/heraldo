37b62dac7728d5f2d10e6342cd135def67f8174f
Jesa Navarro <jesaa@me.com>
2017-04-07 11:13:00 -0500

    Add pagination/categories

--- public/site/templates/_foot.php
+++ public/site/templates/_foot.php
@@ -1,4 +1,5 @@
 <script src="<?php echo $config->urls->templates; ?>static/455375-147357/scripts/main.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
   <script>
     (function() {
         var morphSearch = document.getElementById( 'morphsearch' ),
--- public/site/templates/home.php
+++ public/site/templates/home.php
@@ -1,23 +1,36 @@
 <?php include('./_head.php');
+    $find_category=$input->urlSegment2;
     $field = $fields->get('category');
-    $all_options = $field->type->getOptions($field); ?>
+    $all_options = $field->type->getOptions($field);
+    $option_url = $all_options->get("value=".$find_category);
+    $pagination=10;
+    $cur = $input->pageNum;
+    if(!empty($find_category))
+      $max = intval(($pages->find("template=album, category=".$option_url)->getTotal()) / $pagination);
+    else
+      $max = intval(($pages->find("template=album")->getTotal()) / $pagination);
+    $next = $cur<$max ? $cur + 1 : false;
+    $prev = $cur>1 ? $cur - 1 : false;
+    $ini = $cur==1 ? 0 : ($cur-1)*$pagination;
+    if($cur-1>$max) $session->redirect("/");
+    ?>
   <div class="j-workspace">
      <div class="j-wrap sub-categories-container">
        <ul class="sub-categories">
-       <a href="#">
+       <a href="<?php echo $config->urls->root; ?>">
            <li>Recientes</li>
          </a>
        <?php foreach ($all_options as $option) { ?>
-        <a href="#">
+        <a href="<?php echo $config->urls->root; ?>categoria/<?php echo $option->value; ?>">
            <li><?php echo $option->title; ?></li>
          </a>
         <?php } ?>
        </ul>
-       <select name="categories" id="">
+       <select name="categories" id="categories">
          <option value="Seleccionar">Categorias</option>
          <option value="recientes">Recientes</option>
          <?php foreach ($all_options as $option) { ?>
-         <option value="<?php echo $option->title; ?>"><?php echo $option->title; ?></option>
+         <option value="<?php echo $option->value; ?>"><?php echo $option->title; ?></option>
          <?php } ?>
        </select>
      </div>
@@ -27,7 +40,10 @@
       <h2>Álbumes recientes</h2>
        <div class="grid">
          <!--  Album numero uno-->
-        <?php $albumes=$pages->find("template=album, sort=-published"); 
+        <?php if(!empty($find_category))
+                  $albumes=$pages->find("template=album, sort=-published, category=".$option_url.", start=".$ini.", limit=".$pagination);
+              else
+                  $albumes=$pages->find("template=album, sort=-published, start=".$ini.", limit=".$pagination);
               foreach ($albumes as $album) { ?>             
          <div class="unit one-quarter album-unit">
            <div class="image-album">
@@ -52,3 +68,27 @@
    </div>
 
 <?php include('./_foot.php'); ?>
+<script type="text/javascript">
+$('#categories').change(function() {
+  if($(this).val()=='recientes')
+    window.location = "<?php echo $config->urls->root; ?>";
+  else
+    window.location = "<?php echo $config->urls->root; ?>categoria/"+$(this).val();
+});
+var pagina=1;
+  $(window).scroll(function(){
+    if ($(window).scrollTop() == $(document).height() - $(window).height()){
+      pagina++;
+      document.getElementById("respfotos").value = pagina;
+      cargardatos();
+    }                                       
+  });
+  function cargardatos(){ 
+    $.get("datos?pagina="+pagina,
+      function(data){
+        if (data != "") {
+          $(".grid:last").after(data); 
+        }
+      });                              
+  }
+</script>
