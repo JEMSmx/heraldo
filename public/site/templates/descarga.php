c3dc1d048503e54b67c51e65655464a5a4beace9
Jesa Navarro <jesaa@me.com>
2017-04-23 05:29:31 -0500

    permissions download

--- public/site/templates/descarga.php
+++ public/site/templates/descarga.php
@@ -1,39 +1,46 @@
 <?php
 if(!$user->isLoggedin()) $session->redirect("/"); 
 
-$checksum=k::decrypt(str_replace(" ", "+", $input->get->checksum));
+if($user->hasRole('administrator') || $user->hasRole('superuser') || $user->hasRole('manager')){ 
+	$checksum=k::decrypt(str_replace(" ", "+", $input->get->checksum));
 
-if(strpos($checksum, ":") === false) $session->redirect("/"); 
+	if(strpos($checksum, ":") === false) $session->redirect("/"); 
 
-if(strpos($checksum, "/") === false) $session->redirect("/"); 
+	if(strpos($checksum, "/") === false) $session->redirect("/"); 
 
-$values=explode(":", $checksum);
+	$values=explode(":", $checksum);
 
-$difference=time()-$values[1];
+	$difference=time()-$values[1];
 
-if($difference>10000) $session->redirect("/"); 
+	if($difference>10000) $session->redirect("/"); 
 
-$id_album=explode("/", $values[0]);
+	$id_album=explode("/", $values[0]);
 
-$album=$pages->get($id_album[0]);
+	$album=$pages->get($id_album[0]);
 
-$zip = new ZipArchive();
+	$zip = new ZipArchive();
 
-$filename = $album->name.'.zip';
+	$filename = $album->name.'.zip';
 
-if($zip->open($filename,ZIPARCHIVE::CREATE)===true) {
+	if($zip->open($filename,ZIPARCHIVE::CREATE)===true) {
 
-  foreach ($album->images as $image) {
-    $zip->addFile($image->filename, basename($image->filename));
-  }
+	  foreach ($album->images as $image) {
+	    $zip->addFile($image->filename, basename($image->filename));
+	  }
 
-  $zip->close();
+	  $zip->close();
 
-  if(!$filename) die("download not found");
+	  if(!$filename) die("download not found");
 
-  wireSendFile($filename, array('exit' => false));
-  unlink($filename);
-  exit;
+	  wireSendFile($filename, array('exit' => false));
+	  unlink($filename);
+	  exit;
+	}
+
+}else{
+	$session->redirect("/"); 
 }
+
+
 
 
---
+++ public/site/templates/individual.php
@@ -0,0 +1,35 @@
+<?php
+if(!$user->isLoggedin()) $session->redirect("/"); 
+
+if($user->hasRole('administrator') || $user->hasRole('superuser') || $user->hasRole('manager')){ 
+
+	$checksum=k::decrypt(str_replace(" ", "+", $input->get->checksum));
+
+	if(strpos($checksum, ":") === false) $session->redirect("/"); 
+
+	if(strpos($checksum, "<>") === false) $session->redirect("/"); 
+
+	$values=explode(":", $checksum);
+
+	$difference=time()-$values[1];
+
+	if($difference>10000) $session->redirect("/"); 
+
+	$getFoto=explode("<>", $values[0]);
+
+	$filename=$getFoto[1];
+
+
+	if(!$filename) die("download not found");
+	$download_options = array(
+		'exit' => false,
+		'forceDownload' => true,
+		'downloadFilename' => '',
+		);
+	wireSendFile($filename, $download_options);
+	exit;
+} else{
+	$session->redirect("/"); 
+}
+
+
